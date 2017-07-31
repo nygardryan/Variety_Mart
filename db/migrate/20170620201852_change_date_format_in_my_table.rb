07240782067ec75051be2cc4c403ad6e8ab387d8
Ryan <nygardryan@yahoo.com>
2017-07-31 13:32:06 -0600

    cancelled migration

--- db/migrate/20170620201852_change_date_format_in_my_table.rb
+++
@@ -1,9 +0,0 @@
-class ChangeDateFormatInMyTable < ActiveRecord::Migration[5.1]
-  def up
-    change_column :pawns, :pawn_number, :integer
-  end  
-  def down
-    change_column :pawns, :pawn_number, :string
-  end
-
-end
