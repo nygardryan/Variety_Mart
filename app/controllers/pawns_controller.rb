1d1303bafde07d990a26a81edb4091eaf60ebf4e
Ryan <nygardryan@yahoo.com>
2017-08-08 13:26:19 -0600

    styled for demo

--- app/controllers/pawns_controller.rb
+++ app/controllers/pawns_controller.rb
@@ -73,7 +73,7 @@ class PawnsController < ApplicationController
   def require_login
     unless admin_signed_in?
       flash[:error] = "You must be logged in to access this section"
-      redirect_to new_user_session_path # halts request cycle
+      redirect_to new_admin_session_path # halts request cycle
     end
   end
 
